12f949b5457f2bdb6623b283462ba396efdf58cf
Bannu Akkabattula <bannu@quanbytech.onmicrosoft.com>
2023-02-20 16:04:00 +0530

    splash screen created using material app, theme and scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(
-    Center(
-      child:  Text("Sashi TDP Member of Legislative Assembly", textDirection: TextDirection.ltr)),
-    );
+   MaterialApp(
+        title: "My app",
+        color: Color.fromARGB(255, 0, 0, 0),
+
+    theme: ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+        ),
+        
+   home:Scaffold(
+    appBar: AppBar(
+      title: Text("First screen"),
+      centerTitle: true,
+      backgroundColor: Colors.brown,
+    ),
+
+    floatingActionButton: FloatingActionButton(
+      child: Text("Click" ),
+       onPressed:null
+    ),
+   ),
+   
+ ),
+
+ );
    
 }
